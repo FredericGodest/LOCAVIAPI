99cdea3f23d70722aedf07f652b7c2b5d78a6639
Clement <contact@clement-avenel.com>
2021-03-30 23:33:09 +0200

    Unit tests configuration

--- api-routes.js
+++ api-routes.js
@@ -5,10 +5,6 @@ var port = process.env.PORT || 8080;
 // Initialize express router
 let router = require('express').Router();
 
-const http = require("http")
-const io = require("socket.io");
-const socket = io(http);
-
 // Set default API response
 router.get('/', function (req, res) {
     res.json({
--- tests/advice.test.js
+++ tests/advice.test.js
@@ -1,5 +1,15 @@
-describe('initial test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true)
+const request = require('supertest');
+const app = require('../server')
+
+describe('📄 Testing advices routes', () => {
+  it('should get all advices', async () => {
+    const res = await request(app).get('/advice/all')
+    expect(res.statusCode).toEqual(200)
+  })
+})
+describe('🏠 Testing estates routes', () => {
+  it('should get all estates', async () => {
+    const res = await request(app).get('/estate/all')
+    expect(res.statusCode).toEqual(200)
   })
 })
